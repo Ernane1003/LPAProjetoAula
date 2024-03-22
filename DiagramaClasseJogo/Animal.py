81ab7e9e24de6d89cdb568ce066e78fc3010b599
lima.ernane0@gmail.com <lima.ernane0@gmail.com>
2024-03-21 21:14:08 -0300

    Signed-off-by: lima.ernane0@gmail.com <lima.ernane0@gmail.com>

---
+++ DiagramaClasseJogo/Animal.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Animal:
+    def __init__(self):
+        self.age = None
+        self.height = None
+        self.weight = None
+        self.position = None
+
+    def move_x(self, ):
+        pass
+
+    def move_y(self, ):
+        pass
+
+    def move_z(self, ):
+        pass
+
